5e91cef7812a29b51e1ccbebac28931a7ec4f631
growdu <growdu@gmail.com>
2026-05-15 16:41:54 +0800

    chore: export DataSourceClient, TushareClient, AkShareClient

--- src/aistock/data/sources/__init__.py
+++ src/aistock/data/sources/__init__.py
@@ -1,5 +1,7 @@
 """External data providers."""
 
+from aistock.data.sources.base import DataSourceClient
 from aistock.data.sources.tushare_client import TushareClient
+from aistock.data.sources.akshare_client import AkShareClient
 
-__all__ = ["TushareClient"]
+__all__ = ["DataSourceClient", "TushareClient", "AkShareClient"]
